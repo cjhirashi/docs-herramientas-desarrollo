93eb78082bdbe7a7f338109155a557fb1c97eacc
Carlos Jimenez H. <cjhirashi@gmail.com>
2025-03-02 02:39:34 -0600

    Update document structure: modify links and headings for programming languages and frameworks, and add new items for data management

--- dist/scripts/settings/documents.mjs
+++ dist/scripts/settings/documents.mjs
@@ -1,97 +1,99 @@
 export const Documents = [
     {
         title: "Introducción",
-        href: "/introduction",
-        heading: "Comenzando",
+        href: "/documentation",
+        heading: "Sobre la documentación",
         items: [
             {
                 title: "Instalación",
                 href: "/installation",
             },
-            {
-                title: "Configuración",
-                href: "/setup",
-            },
-            {
-                title: "Registro de cambios",
-                href: "/changelog",
-            },
         ],
     },
     {
         spacer: true,
     },
     {
-        title: "Navegación",
-        href: "/navigation",
-        heading: "Documentos",
-    },
-    {
-        title: "Esctructura",
-        href: "/structure",
+        title: "Introducción",
+        href: "/languages",
+        heading: "Lenguajes de programación",
         items: [
             {
-                title: "Profundo",
-                href: "/deep",
+                title: "Python",
+                href: "/python",
+            },
+            {
+                title: "Javascript",
+                href: "/javascript",
+            },
+            {
+                title: "Typescript",
+                href: "/typescript",
+            },
+            {
+                title: "Dart",
+                href: "/dart",
+            },
+            {
+                title: "Rikmed (Mircom)",
+                href: "/rikmed",
+            },
+            {
+                title: "BestPP (KMC)",
+                href: "/bestpp",
+            },
+        ]
+    },
+    {
+        spacer: true,
+    },
+    {
+        title: "Introducción",
+        href: "/frameworks",
+        heading: "Frameworks de Aplicación",
+        items: [
+            {
+                title: "Next JS",
+                href: "/nextjs",
+            },
+        ]
+    },
+    {
+        spacer: true,
+    },
+    {
+        title: "Introducción",
+        href: "/data-management",
+        heading: "Gestión de datos",
+        items: [
+            {
+                title: "Bases de datos",
+                href: "/databases",
                 items: [
                     {
-                        title: "Más profundo",
-                        href: "/deeper",
-                        items: [
-                            {
-                                title: "Aún más profundo",
-                                href: "/even-deeper",
-                            },
-                        ],
+                        title: "SQL",
+                        href: "/sql",
                     },
-                ],
-            },
-        ],
-    },
-    {
-        spacer: true,
-    },
-    {
-        title: "Markdown",
-        href: "/markdown",
-        heading: "Componentes",
-        items: [
-            {
-                title: "Tarjetas",
-                href: "/cards",
+                    {
+                        title: "PostgreSQL",
+                        href: "/postgres",
+                    },
+                    {
+                        title: "MongoDB",
+                        href: "/mongodb",
+                    },
+                ]
             },
             {
-                title: "Diagramas",
-                href: "/diagrams",
+                title: "Plataformas",
+                href: "/platforms",
+                items: [
+                    {
+                        title: "Supabase",
+                        href: "/supabase",
+                    },
+                ]
             },
-            {
-                title: "Arbol de archivos",
-                href: "/filetree",
-            },
-            {
-                title: "Listas",
-                href: "/lists",
-            },
-            {
-                title: "Matemáticas",
-                href: "/maths",
-            },
-            {
-                title: "Notas",
-                href: "/notes",
-            },
-            {
-                title: "Pasos",
-                href: "/steps",
-            },
-            {
-                title: "Tablas",
-                href: "/table",
-            },
-            {
-                title: "Tabs",
-                href: "/tabs",
-            },
-        ],
+        ]
     },
 ];
