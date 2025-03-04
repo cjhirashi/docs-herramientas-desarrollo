08e3952c604164694aea13590048a6c3387716d6
Carlos Jimenez H. <cjhirashi@gmail.com>
2025-03-03 23:53:39 -0600

    Add Python programming section with detailed topics and navigation

--- settings/documents.ts
+++ settings/documents.ts
@@ -27,6 +27,36 @@ export const Documents: Paths[] = [
           {
             title: "Python",
             href: "/python",
+            items: [
+              {
+                title: "Tipos de tados",
+                href: "data-types"
+              },
+              {
+                title: "Estructuras de control",
+                href: "control-structure"
+              },
+              {
+                title: "Funciones",
+                href: "/functions"
+              },
+              {
+                title: "Programación orientada a objetos",
+                href: "/oop"
+              },
+              {
+                title: "Módulos y paquetes",
+                href: "/modules-packages"
+              },
+              {
+                title: "Manejo de excepciones",
+                href: "/exceptions"
+              },
+              {
+                title: "Bibliotecas de programación",
+                href: "/libraries"
+              },
+            ]
           },
           {
             title: "Javascript",
